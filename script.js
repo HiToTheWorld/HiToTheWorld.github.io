e5eab01f55c367b7fde6e83ee58d69e7b266eef9
rabitailleow <93296145+rabitailleow@users.noreply.github.com>
2022-07-19 12:16:04 -0400

    Updated projects

---
+++ script.js
@@ -0,0 +1,24 @@
+const navbar = document.getElementById('navbar')
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function showProjectModal(pelem) {
+  let modal = pelem.getElementsByClassName('projectModal')[0]
+  modal.style.display = "block"
+}
+
+document.addEventListener('scroll', function() {
+  if (isInViewport(navbar) == true) {
+
+  } else {
+
+  }
+})
