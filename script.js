720ccf848641a21f6c426323b97bb87f1214f24e
rabitailleow <93296145+rabitailleow@users.noreply.github.com>
2022-07-20 09:03:18 -0400

    fsefd

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const navbar = document.getElementById('navbar')
 const navbarcover = document.getElementById('navbarcover')
 
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+// function isInViewport(element) {
+//   const rect = element.getBoundingClientRect();
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// }
 
-function showProjectModal(pelem) {
-  let modal = pelem.getElementsByClassName('projectModal')[0]
-//   modal.style.display = "block"
-}
+// function showProjectModal(pelem) {
+//   let modal = pelem.getElementsByClassName('projectModal')[0]
+// //   modal.style.display = "block"
+// }
 
 let side = true
 
@@ -45,3 +45,5 @@ document.onscroll = function () {
     
   }
 }
+
+console.log('finished compiling')
