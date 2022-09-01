b3adf95b6d8a73b3f3ed18c285653e490cfa343e
Owen Rabitaille <rabitailleow@gmail.com>
2022-08-31 20:12:20 -0400

    flipswitch

---
+++ projects/flipswitch/script.js
@@ -0,0 +1,34 @@
+document.getElementById("enablejs").style.display = "none"
+
+window.onorientationchange = function() {
+  window.location.reload()
+}
+
+if (window.orientation == 0) {
+  document.getElementById("rotatedevice").style.display = "flex"
+}
+
+function onstart() {
+  gameplay()
+}
+
+//GAMEPLAY CONFIG
+let cardgroups = {
+    "hot": {title: "Hot", ptnr: "cold", pic: "cards/hot.png"},
+    "cold": {title: "Cold", ptnr: "hot", pic: "cards/cold.png"}
+}
+let cards = [
+    {title: "Flamin' Hot Cheatos", group: "hot"}
+]
+
+function gameplay() {
+    function side1() {
+
+    }
+
+    function side2() {
+
+    }
+}
+
+document.getElementById("start").addEventListener("click", onstart)
