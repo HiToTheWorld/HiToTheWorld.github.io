7066c076d6eefb58616483175aaab93a8843e048
Owen Rabitaille <rabitailleow@gmail.com>
2022-09-01 21:22:17 -0400

    flipswitch score update

--- projects/flipswitch/script.js
+++ projects/flipswitch/script.js
@@ -1,6 +1,6 @@
 document.getElementById("enablejs").style.display = "none"
 
-window.onorientationchange = function() {
+window.onorientationchange = function () {
   window.location.reload()
 }
 
@@ -12,24 +12,101 @@ function onstart() {
   gameplay()
 }
 
+//SELF CONFIG - DO NOT TOUCH
+let lassign = "a"
+let rassign = "l"
+
 //GAMEPLAY CONFIG
 let cardgroups = {
-    "hot": {title: "Hot", ptnr: "cold", pic: "cards/hot.png"},
-    "cold": {title: "Cold", ptnr: "hot", pic: "cards/cold.png"}
+  "hot": { title: "Hot", ptnr: "cold", pic: "images/groups/hot.png" },
+  "cold": { title: "Cold", ptnr: "hot", pic: "images/groups/cold.png" },
+
+
 }
 let cards = [
-    {title: "Flamin' Hot Cheatos", group: "hot"},
-    {title: "Snow", group: "cold"}
+  { title: "Flamin' Hot Cheatos", group: "hot", pic: "images/cards/fhc.png" },
+  { title: "Snow", group: "cold", pic: "images/cards/snow.png" }
+]
+let powerups = [
+  {
+    title: "Flip", pic: "images/powerups/flip.png", f: function () {
+      let newgroups = [currentGroups[1], currentGroups[0]]
+      currentGroups = newgroups
+    }
+  },
+  {
+    title: "Switch", pic: "images/powerups/switch.png", f: function () {
+      assignGroups()
+    }
+  },
+  {
+    title: "Cross", pic: "images/powerups/cross.png", f: function () {
+      lassign = false
+      rassign = true
+    }
+  }
 ]
 
 function gameplay() {
-    function side1() {
-        
-    }
+  let currentCard
+  let currentGroups = []
+  function clearpowerups() {
+    lassign = true
+    rassign = false
+  }
 
-    function side2() {
+  function generateCard() {
+    currentCard = cards[Math.floor(Math.random() * cards.length)]
+    document.getElementById("ccardimage").style.backgroundImage = "url('" + currentCard.pic + "')";
+    document.getElementById("ccardtitle").innerText = currentCard.title;
+  }
 
+  function assignGroups() {
+    let cooldown = 0
+    let newGroup0 = ""
+    do {
+      newGroup0 = Object.keys(cardgroups)[Math.floor(Math.random() * Object.keys(cardgroups).length)]
+      cooldown += 1
+    } while ((newGroup0 == currentGroups[0] || newGroup0 == currentGroups[1]) && cooldown < 1000)
+    currentGroups[0] = newGroup0
+    currentGroups[1] = cardgroups[currentGroups[0]].ptnr
+    document.getElementById("side1image").style.backgroundImage = "url('" + cardgroups[currentGroups[0]].pic + "')";
+    document.getElementById("side1title").innerText = cardgroups[currentGroups[0]].title;
+    document.getElementById("side2image").style.backgroundImage = "url('" + cardgroups[currentGroups[1]].pic + "')";
+    document.getElementById("side2title").innerText = cardgroups[currentGroups[1]].title;
+  }
+
+  function side1() {
+    if (currentCard.group == currentGroups[0]) {
+      console.log("Correct!1")
     }
+    generateCard()
+  }
+
+  function side2() {
+    if (currentCard.group == currentGroups[1]) {
+      console.log("Correct!2")
+    }
+    generateCard()
+  }
+
+  function oninputfunction(inputKey) {
+    if (inputKey == lassign) {
+      side1()
+    } else if (inputKey == rassign) {
+      side2()
+    }
+  }
+
+  assignGroups()
+  generateCard()
+
+  document.getElementById("side1image").addEventListener("mousedown", function (e) { oninputfunction(lassign) })
+  document.getElementById("side2image").addEventListener("mousedown", function (e) { oninputfunction(rassign) })
+
+  window.addEventListener("keydown", function (e) { oninputfunction(e.key) })
 }
+
+gameplay()
 
 document.getElementById("start").addEventListener("click", onstart)
