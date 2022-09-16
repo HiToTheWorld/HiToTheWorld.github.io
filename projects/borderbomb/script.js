681491891b27443701a671c44f1788f85375cffe
rabitailleow <93296145+rabitailleow@users.noreply.github.com>
2022-09-16 08:57:45 -0400

    Update script.js

--- projects/borderbomb/script.js
+++ projects/borderbomb/script.js
@@ -2,13 +2,16 @@ let imageInput = document.getElementById("imageInput")
 let imageDisplay = document.getElementById("imageDisplay")
 let file = imageInput.value;
 
-const globalX = 300
-const globalY = 300
+let buildStopped = false
+
+let globalX = 900, globalY = 900
 
 imageInput.addEventListener("change", function() {
   file = URL.createObjectURL(imageInput.files[0]);
   imageDisplay.src = file
 })
+
+
 
 let x = 1
 let y = 1
@@ -51,22 +54,40 @@ function inProg(num) {
 }
 
 function onSub() {
+  document.getElementById("stopBuild").disabled = false
+  document.getElementById("clearBuild").disabled = false
+  document.getElementById("imageInput").disabled = true
+  document.getElementById("borderWidth").disabled = true
+  document.getElementById("borderColor").disabled = true
+  document.getElementById("imageRes").disabled = true
+  document.getElementById("imageResLabel").disabled = true
+  document.getElementById("start").disabled = true
   clear()
+
+  globalX, globalY = document.getElementById("imageRes").value
 
   let image = new Image(globalX, globalY)
   image.src = file
   let canvas = document.createElement("canvas")
   let ctx = canvas.getContext('2d')
-  ctx.drawImage(image, 0, 0, globalX, globalY);
 
-  canvas.width = image.width
-  canvas.height = image.height
+  canvas.width = globalX
+  canvas.height = globalY
+  ctx.drawImage(image, 0, 0, globalX, globalY);
 
   let outcanvas = document.getElementById("outCanvas")
   let outctx = outcanvas.getContext('2d')
 
+  outcanvas.width = globalX
+  outcanvas.height = globalY
+
   outctx.fillStyle = document.getElementById("borderColor").value;
   outctx.strokeStyle = document.getElementById("borderColor").value;
+  let borderwidth = document.getElementById("borderWidth").value;
+
+  if (document.getElementById("showImageOnOut").checked == true) {
+    outctx.drawImage(image, 0, 0, globalX, globalY);
+  }
 
   function getAllData() {
     let data = ctx.getImageData(0, 0, globalX, globalY).data
@@ -74,50 +95,62 @@ function onSub() {
   }
 
   let data = Array.from(getAllData())
-  // let pxData = []
-  // let lineData = []
 
-  document.getElementById("borderProgress").max = globalX * globalY
+  document.getElementById("borderProgress").max = data.length / 4
 
   let isNewLinePx = true
   let lastpx = null
 
   let line = 0
 
+  let cLine = []
+  let lastLine = null
+
   function newLin() {
     isNewLinePx = true
-    let pixel = 0
+    let pixel = 1
+    lastLine = cLine
+    cLine = []
 
     function newPix() {
       let px = data.splice(0, 4)
-      if (lastpx != null) {
-        console.log(lastpx)
+      let pxloc = pixel - 1
+      if (lastpx != null && lastLine.length != 0) {
         if (lastpx[3] != 0 && px[3] == 0) {
-          outctx.fillRect(pixel, line, 1, 1)
+          outctx.fillRect(pxloc - (borderwidth / 2), line - (borderwidth / 2), 1 + (borderwidth / 2), 1 + (borderwidth / 2))
         } else if (lastpx[3] == 0 && px[3] != 0) {
           if (isNewLinePx == false) {
-            outctx.fillRect(pixel, line, 1, 1)
+            outctx.fillRect(pxloc - 1 - (borderwidth / 2), line - (borderwidth / 2), 1 + (borderwidth / 2), 1 + (borderwidth / 2))
           }
+        } else if (lastLine[pxloc][3] != 0 && px[3] == 0) {
+          outctx.fillRect(pxloc - (borderwidth / 2), line - (borderwidth / 2), 1 + (borderwidth / 2), 1 + (borderwidth / 2))
+        } else if (lastLine[pxloc][3] == 0 && px[3] != 0) {
+          outctx.fillRect(pxloc - (borderwidth / 2), line - 1 - (borderwidth / 2), 1 + (borderwidth / 2), 1 + (borderwidth / 2))
         }
       }
 
       lastpx = px
       isNewLinePx = false
 
+      cLine.push(px)
+
       pixel += 1
       inProg(1)
-      if (pixel < globalX) {
+
+      if (pixel <= globalX && buildStopped == false) {
         window.setTimeout(newPix, 0.0001)
+      } else {
+      	line += 1
+        if (line <= globalY && buildStopped == false) {
+          window.setTimeout(newLin, 0.0001)
+        } else {
+          clear()
+          log("Projection complete")
+          document.getElementById("stopBuild").disabled = true
+        }
       }
     }
     newPix()
-
-    if (line < globalY) {
-      window.setTimeout(newLin, 0.0001)
-    } else {
-      clear()
-      log("Projection complete")
-    }
   }
 
   // function sortLines() {
