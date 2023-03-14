fb9c330555939b066ed0d9517b528340634a8983
rabitailleow <93296145+rabitailleow@users.noreply.github.com>
2023-03-14 19:33:06 -0400

    Update data.js

--- projects/washiez/resources/data.js
+++ projects/washiez/resources/data.js
@@ -1,8 +1,8 @@
 const data = {
-    "How do you become a Shift Leader?": ["First, do you meet the following requirements?\\✅ Earn 250 skill\\✅ Regularly play the game\\✅ Display professionalism, respect for others, and the ability to effectively handle situations at the car wash\\✅ Work as a staff member for a minimum of 2 weeks\\If so, you can be promoted through the recommendation process.", ["#promotion"]],
+    "How do you become a Shift Leader?": ["First, do you meet the following requirements?\\✅ Earn 250 skill\\✅ Regularly play the game\\✅ Display professionalism, respect for others, and the ability to effectively handle situations at the car wash\\✅ Work as a staff member for a minimum of 2 weeks\\If so, you can be promoted through the recommendation process.", ["#promotion", "#ST"]],
     "How do you get skill points?": ["You can get skill points by working at the booths!", []],
-    "Where are the training guides?": ["You can find the training guides in the #supervision-information channel in the Communications Server!", []],
-    "How do I become staff?": ["First, join the Washiez group!\\Head to the application center to take the Employee Test!\\Then, attend trainings to work your way up the ranks!", []],
+    "Where are the training guides?": ["You can find the training guides in the #supervision-information channel in the Communications Server!", ["#training"]],
+    "How do I become staff?": ["First, join the Washiez group!\\Head to the application center to take the Employee Test!\\Then, attend trainings to work your way up the ranks!", ["#staff", "#trainee"]],
 }
 
 export default data;
