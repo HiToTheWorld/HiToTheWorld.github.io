b7e7f28bfbddcad2cfd3392d7db43050c1602b39
rabitailleow <93296145+rabitailleow@users.noreply.github.com>
2023-03-16 10:14:27 -0400

    Update data.js

--- projects/washiez/resources/data.js
+++ projects/washiez/resources/data.js
@@ -4,7 +4,7 @@ const data = {
     "Where are the training guides?": ["You can find the training guides in the #supervision-information channel in the Communications Server!", ["#training"]],
     "How do I become staff?": ["First, join the Washiez group!\\Head to the application center to take the Employee Test!\\Then, attend trainings to work your way up the ranks!", ["#staff", "#trainee"]],
     "How do I call an ST or an HR?": ["You can call an ST by saying '-Calls ST-' in chat.\\An alternative is calling in the Communications server, where you can call HR too.", ["#staff"]],
-    "Can I give overhead warnings?": ["**If the staff member is ST+**\\You can warn clients by clicking the staff panel button (the safety symbol).\\Then, enter the user information.\\**If the staff member is not ST+**\\Unfortunately due to your rank, you are unable to warn people with overhead warnings.\\You can still give a verbal warning, then report to ST+ for assistants.", ["#staff", "#warnings"]],
+    "Can I give overhead warnings?": ["**If the staff member is ST+\\You can warn clients by clicking the staff panel button (the safety symbol).\\Then, enter the user information.\\**If the staff member is not ST+\\Unfortunately due to your rank, you are unable to warn people with overhead warnings.\\You can still give a verbal warning, then report to ST+ for assistants.", ["#staff", "#warnings"]],
 }
 
 export default data;
